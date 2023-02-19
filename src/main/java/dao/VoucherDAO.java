e7175ba515408518912451ef7e708a2ed9fb95d2
tuikjn <93249982+tuikjn@users.noreply.github.com>
2023-02-19 19:18:10 +0700

    ok

---
+++ src/main/java/dao/VoucherDAO.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import context.DBContext;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import model.Voucher;
+
+/**
+ *
+ * @author admin
+ */
+public class VoucherDAO {
+/**
+     * Function get list Voucher
+     *
+     * This function use to take Voucher
+     * to upload to the home page
+     *
+     * @return List<Voucher> list of Voucher
+     */
+    public static List<Voucher> getAllVoucher() {
+
+        Connection con = null;       // connect to SQL server.
+        PreparedStatement ps = null; // move query from Netbeen to SQl.
+        ResultSet rs = null;         // save result query.
+        List<Voucher> list = new ArrayList<>();
+        try {
+            //Declare a SQL query string
+            String query = "SELECT * FROM VOUCHER";
+
+            con = new DBContext().getConnection(); //Open connection to SQL
+            ps = con.prepareStatement(query); //Move query to database
+            rs = ps.executeQuery();
+            //Initialize a new user object
+            //Loop through the result set and create a new user object with the retrieved data
+            while (rs.next()) {
+                //Create a new User object using data retrieved from the database
+                list.add(new Voucher(rs.getString(1), rs.getString(2), rs.getString(3), rs.getInt(4)));
+            } //End while
+            //Return the user object
+
+        } catch (Exception e) {
+            e.getMessage();
+        } //End trycatch
+        //If an exception is caught, return null
+        return list;
+    }
+
+}
